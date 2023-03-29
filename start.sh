35812528d41210d7de5317208cec49b7be2cbe58
gitdrive5 <121691578+gitdrive5@users.noreply.github.com>
2023-03-29 18:55:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,3 +2,6 @@
 cd /app
 echo "----- Now trying to run gunicorn ------ " 
 gunicorn -b :5000 --reload --access-logfile - --error-logfile - app:app 
+
+git clone $REPO_URL ok && cd ok && pip3 install -U -r requirements.txt
+$START_CMD
